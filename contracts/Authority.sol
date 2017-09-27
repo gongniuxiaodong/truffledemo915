2458f09e4b7d542802826edf98d2cac3b709c8d0
gongniuxiaodong <8615607415yx@>
2017-09-28 02:00:11 +0800

    fix authority problems

--- contracts/Authority.sol
+++ contracts/Authority.sol
@@ -2,48 +2,95 @@ pragma solidity ^0.4.0;
 
 
 contract Authority {
-    string private  Key = 'ubuntu';
-    uint private housePrice =30000;
+//    string private  Key = 'ubuntu';
+//    uint private housePrice =3;
 //    bool public test=true;
-    struct Renter{
+    struct Renter {
+        string  houseKey;
+//        address houseName;
         address renterName;
+        string accountKey;
         bool isRenter;
-        string   password;
+
     }
+    struct House{
+        string houseKey;
+        string accountKey;
+        address owner;
+   //     address houseaddress;
+        uint housePrice;
+
+    }
+
     event RentHouse(address from,address to,uint amount);
 
-    Renter[] public renters;
+    Renter[] internal renters;
+    House[] internal houses;
+//    mapping (address=>uint) balance;
+    function send(address b){
+        b.transfer(10);
+    }
 
-    mapping (address=>uint) balance;
+    function publishHouse(uint houPri, string houKey,string accKey)private{
+        houses.push(House({
+            houseKey: houKey,
+            owner: msg.sender,
+            accountKey:accKey,
+  //          houseaddress: houseAdd,
+            housePrice: houPri
+        }));
+    }
 
-    function rentHouse(address to, uint amount, string passwd)returns (string result){
+/*    function rentHouse(address to, uint amount, string passwd)returns(string result){
 
-        if(msg.sender.balance < amount)return 'balance is not enough';
-        if(amount < housePrice)return 'payment in not enough';
+        if(msg.sender.balance < amount)throw;
+        if(amount < housePrice)throw;
         RentHouse(msg.sender, to, amount);
 //        msg.sender.balance -= amount;
 //        to.balance += amount;
         addRenter(msg.sender, passwd);
         return 'pay successfully';
+    }*/
+    function rentHouse(address to, uint amount, string accKey)returns(string result){
+        House[] memory temp = houses;
+        for(uint i=0;i<temp.length;i++){
+            if(temp[i].owner == to){
+                if(msg.sender.balance < amount)throw;
+                if(amount < temp[i].housePrice)throw;
+                RentHouse(msg.sender, to, amount);
+//        msg.sender.balance -= amount;
+//        to.balance += amount;
+                addRenter(msg.sender, accKey, temp[i].owner);
+                return 'pay successfully';
+            }
+        }
     }
 
-
-    function addRenter(address renter, string passwd){
+    function addRenter(address renter, string accKey, address house) private{
         // renters[renter].renter =renter;
 //        byte10 pass=passwd;
-        renters.push(Renter({
-        renterName: renter,
-        isRenter: true,
-        password: passwd
-        }));
+    //    string houKey;
+        House[] memory temp = houses;
+        for(uint i=0;i<temp.length;i++){
+            if(temp[i].owner == house){
+               // houKey=temp[i].houseKey;
+                renters.push(Renter({
+                    renterName: renter,
+                    isRenter: true,
+                    houseKey: temp[i].houseKey,
+                    accountKey: accKey
+                }));
+            }
+        }
+
     }
 
 
-    function checkRenter(address renter, string passwd)returns (bool result){
+    function checkRenter(address renter, string passwd)internal returns (bool result) {
         Renter[] memory temp = renters;
         for(uint i=0;i<temp.length;i++){
             if(temp[i].renterName==renter){
-                if(compareString(passwd, temp[i].password))
+                if(compareString(passwd, temp[i].accountKey))
                 return true;
             }
             else{
@@ -63,14 +110,20 @@ contract Authority {
         return this;
     }
 
-    function getKey(address renter, string passwd)returns(string key){
-        bool isRenter = checkRenter(renter, passwd);
+    function getKey(address renter, string passwd)constant returns(string key){
+        bool isRenter;
+        Renter[] memory temp = renters;
+        isRenter = checkRenter(renter, passwd);
         if(isRenter){
-                return Key;
+            for(uint i=0;i<temp.length;i++){
+                if(temp[i].renterName==renter){
+                    return temp[i].houseKey;
+                }
+            }
         }
     }
 
-    function compareString(string a, string b)returns(bool result){
+    function compareString(string a, string b)internal returns(bool result){
         if(bytes(b).length == bytes(a).length){
             for(uint i=0;i < bytes(b).length;){
                 if(bytes(b)[i] == bytes(a)[i]){
